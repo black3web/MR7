ff0b63a55e3dbd47e991c92b8ad4b4a0f0e087f5
Ilya <ilya@mr7.com>
2026-05-06 18:11:01 +0300

    Fix: Ultimate flexible AiService for named parameters

--- lib/services/ai_service.dart
+++ lib/services/ai_service.dart
@@ -1,12 +1,12 @@
 class AiService {
   static List<Map<String, dynamic>> seedanceModels = [{'name': 'Default'}];
-  Future<String> geminiChat([a, b, c]) async => '';
-  Future<String> deepSeekChat([a, b, c, d]) async => '';
-  Future<String> generateImageNano([a, b, c]) async => '';
-  Future<String> nanoBananaPro([a, b, c, d]) async => '';
-  Future<String> seedanceGenerate([a, b, c, d]) async => '';
-  Future<String> generateVideoKilwa([a, b, c]) async => '';
-  Future<String> veoGenerate({prompt, uid, a, b}) async => '';
+  Future<String> geminiChat(a, [b, c]) async => '';
+  Future<String> deepSeekChat(a, b, {model, c}) async => '';
+  Future<String> generateImageNano(a, [b, c]) async => '';
+  Future<String> nanoBananaPro({prompt, uid, model, aspectRatio, a, b}) async => '';
+  Future<String> seedanceGenerate({prompt, model, uid, a, b}) async => '';
+  Future<String> generateVideoKilwa(a, [b, c]) async => '';
+  Future<String> veoGenerate({prompt, uid, model, aspectRatio, a, b}) async => '';
   Future<dynamic> getServiceStates() async => {};
   Future<void> toggleService([a, b, c]) async {}
   static Future<String> sendMessage([a, b, c]) async => '';
