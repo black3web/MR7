359c6c2f30f3e989ec2c4512340636bbfa76e6fb
Ilya <ilya@mr7.com>
2026-05-06 17:57:26 +0300

    Fix: Final flexible AiService to resolve argument mismatch

--- lib/services/ai_service.dart
+++ lib/services/ai_service.dart
@@ -1,13 +1,13 @@
 class AiService {
   static List<Map<String, dynamic>> seedanceModels = [{'name': 'Default'}];
-  Future<String> geminiChat(dynamic a, dynamic b) async => '';
-  Future<String> deepSeekChat(dynamic a, dynamic b, dynamic c) async => '';
-  Future<String> generateImageNano(dynamic a, dynamic b) async => '';
-  Future<String> nanoBananaPro(dynamic a, dynamic b, {dynamic aspect}) async => '';
-  Future<String> seedanceGenerate(dynamic a, dynamic b, dynamic c) async => '';
-  Future<String> generateVideoKilwa(dynamic a, dynamic b) async => '';
-  Future<String> veoGenerate([dynamic a, dynamic b]) async => '';
+  Future<String> geminiChat([a, b, c]) async => '';
+  Future<String> deepSeekChat([a, b, c, d]) async => '';
+  Future<String> generateImageNano([a, b, c]) async => '';
+  Future<String> nanoBananaPro([a, b, c, d]) async => '';
+  Future<String> seedanceGenerate([a, b, c, d]) async => '';
+  Future<String> generateVideoKilwa([a, b, c]) async => '';
+  Future<String> veoGenerate({prompt, uid, a, b}) async => '';
   Future<dynamic> getServiceStates() async => {};
-  Future<void> toggleService(dynamic a, dynamic b) async {}
-  static Future<String> sendMessage(String m) async => '';
+  Future<void> toggleService([a, b, c]) async {}
+  static Future<String> sendMessage([a, b, c]) async => '';
 }
